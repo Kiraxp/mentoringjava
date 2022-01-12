8e0a2d256e3fa288c94294d58958161fcfe52c10
LAPTOP-FKFB2JOM\Kiraxp <sulistyo707@gmail.com>
2022-01-12 09:16:01 +0700

    Commit pertama

---
+++ src/main/java/com/slstyo/lmsku/LmskuApplication.java
@@ -0,0 +1,13 @@
+package com.slstyo.lmsku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmskuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmskuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slstyo/lmsku/LmskuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slstyo.lmsku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmskuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
